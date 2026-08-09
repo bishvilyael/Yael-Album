52cfcd177f94dd718e7d490f561032e4d96aa2e2
bishvilyael <bishvil.yael@gmail.com>
2026-08-09 18:46:47 +0300

    Update GOOGLE_CLIENT_ID in config.js
    Updated GOOGLE_CLIENT_ID to the correct value.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// Yael PWA configuration
-// Replace GOOGLE_CLIENT_ID with the Web OAuth Client ID from Google Cloud Console.
+
+// Yael Album PWA configuration
 window.YAEL_CONFIG = {
   ROOT_FOLDER_ID: '1g-ijAmD2LeRLU6d4AIR52aZrKy0XK2Ov',
-  GOOGLE_CLIENT_ID: 'PASTE_GOOGLE_WEB_CLIENT_ID_HERE.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '104105233546-s0vbdlosudecs5eiq6dvtqp7er52n62r.apps.googleusercontent.com',
   APP_NAME: 'יעל – צפייה בתמונות'
 };
