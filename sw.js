f537ca8d9819e382edd6b5f58c7ed26e639faf21
bishvilyael <bishvil.yael@gmail.com>
2026-08-09 19:14:58 +0300

    Update cache version to 0.1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yael-pwa-0.1.0';
+const CACHE='yael-pwa-0.1.1';
 const ASSETS=['./','index.html','styles.css','app.js','config.js','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png','icons/icon-180.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
