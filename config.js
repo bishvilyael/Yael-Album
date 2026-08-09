9da9ce9e1dae957d0bb7c7f27648df9fcb3af6c9
bishvilyael <bishvil.yael@gmail.com>
2026-08-09 18:56:31 +0300

    Update GOOGLE_CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Yael Album PWA configuration
 window.YAEL_CONFIG = {
   ROOT_FOLDER_ID: '1g-ijAmD2LeRLU6d4AIR52aZrKy0XK2Ov',
-  GOOGLE_CLIENT_ID: '104105233546-s0vbdlosudecs5eiq6dvtqp7er52n62r.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '104105233546-s0vbdlosudecs5eig6dvtqp7er52n62r.apps.googleusercontent.com',
   APP_NAME: 'יעל – צפייה בתמונות'
 };
